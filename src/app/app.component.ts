cb63bd2bfd6c94431d9ef7410506b5052e8f824a
scottire <condronscott@gmail.com>
2018-05-30 21:00:32 +0200

    Add MNIST model and use to predict values
    - Follow remainder of tutorial to add MNIST dataset stuff
    - Copied the model, drawing canvas and the dependencies stuff from the remainder of the tutorial
    - Add the chart to index.html
    - Link to tutorial: https://angularfirebase.com/lessons/tensorflow-js-quick-start/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit, ViewChild} from '@angular/core'
 import * as tf from '@tensorflow/tfjs'
-import {Tensor} from "@tensorflow/tfjs"
+import { DrawableDirective } from './drawable.directive'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  predictions: number[]
+  model: tf.Model
   linearModel: tf.Sequential
-  prediction: any
+  @ViewChild(DrawableDirective) canvas;
+  private prediction: number;
 
   async ngOnInit() {
+    await this.loadModel()
     await this.trainNewModel()
   }
 
   private async trainNewModel() {
     this.linearModel = tf.sequential()
-    this.linearModel.add(tf.layers.dense({units: 1, inputShape:[1]}))
+    this.linearModel.add(tf.layers.dense({units: 1, inputShape: [1]}))
     this.linearModel.compile({loss: 'meanSquaredError', optimizer: 'sgd'})
 
     const xs = tf.tensor1d([3.2, 4.4, 5.5, 6.71, 6.98, 7.168, 9.779, 6.182, 7.59, 2.16, 7.042, 10.71, 5.313, 7.97, 5.654, 9.7, 3.11])
@@ -29,7 +34,23 @@ export class AppComponent implements OnInit {
   }
 
   linearPrediction(val: number){
-    const output = this.linearModel.predict(tf.tensor2d([val], [1, 1])) as Tensor
+    const output = this.linearModel.predict(tf.tensor2d([val], [1, 1])) as tf.Tensor
     this.prediction = Array.from(output.dataSync())[0]
+  }
+
+
+  private async loadModel() {
+    this.model = await tf.loadModel('/assets/model.json')
+  }
+
+  async predict(imageData: ImageData) {
+    const pred = tf.tidy(() => {
+      let img = tf.fromPixels(imageData, 1)
+      img = img.reshape([1, 28, 28, 1] as any)
+      img = tf.cast(img, 'float32')
+      const output = this.model.predict(img) as tf.Tensor
+      // Save predictions on the component
+      this.predictions = Array.from(output.dataSync());
+    })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+
+import { AppComponent } from './app.component'
+import { ChartsModule } from 'ng2-charts/ng2-charts'
+
+import { DrawableDirective } from './drawable.directive';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    DrawableDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
